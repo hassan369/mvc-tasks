9742c7e6d3124e06f9562a5ca9066413e6652904
hassan ayasrah <hassanayasrah68@gmail.com>
2023-01-31 16:47:13 +0300

    task1 mvc

---
+++ 31-1-2023/WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string image()
+        {
+            return ("<a download='juce.png' href='~/images/juce.png'><img src='../images/juce.png'></a>");
+
+        }
+
+        public ActionResult AboutUs()
+        {
+            return Content("About");
+        }
+        public ActionResult ContactUs()
+        {
+            return Content("Contact");
+        }
+    }
+}
---
+++ 31-1-2023/WebApplication1/WebApplication1/Controllers/HassanController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HassanController : Controller
+    {
+        // GET: Hassan
+        public ActionResult actoin1()
+        {
+            return Content("Action1");
+        }
+        public ActionResult actoin2()
+        {
+            return Content("Action2");
+        }
+        public ActionResult actoin3()
+        {
+            return Content("Action3");
+        }
+        public ActionResult actoin4()
+        {
+            return Content("Action4");
+        }
+    }
+}
