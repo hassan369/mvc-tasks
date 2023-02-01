0e6277784e265642afdc2917c52cfdff3a4258f6
hassan ayasrah <hassanayasrah68@gmail.com>
2023-02-01 16:47:52 +0300

    task 1-2-2023

---
+++ 1-2-2023/WebApplication1/WebApplication1/Controllers/task1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class task1Controller : Controller
+    {
+        // GET: task1
+        public ActionResult Index()
+        {
+            List<Models.Student> student = new List<Models.Student>();
+            student.Add(new Models.Student{ID = 1, Name = "hassan", Major = "computer science" , Faculity="it" });
+            student.Add(new Models.Student { ID = 2, Name = "odat", Major = "electrical engenering", Faculity = "engening" });
+            ViewBag.Student = student;
+            return View();
+        }
+    }
+}
---
+++ 1-2-2023/WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public string Faculity { get; set; }
+    }
+}
