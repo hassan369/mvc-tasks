add6119e72ecf77bbb5651f5d427e3986e30dfa6
hassan ayasrah <hassanayasrah68@gmail.com>
2023-02-12 15:29:00 +0300

    adding task 12-2-2023

---
+++ 12-2-2023/testCodeFirst/testCodeFirst/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace testCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<testCodeFirst.Data.testCodeFirstContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(testCodeFirst.Data.testCodeFirstContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
